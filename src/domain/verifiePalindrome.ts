c283ac2479904e7f30f38843c0766a9bbd1a8a56
laura-nesta <laura.nesta@outlook.fr>
2024-01-31 19:52:56 +0100

    Etape2 - update constructeur VerifiePalindrome + signature Verifie

--- src/domain/verifiePalindrome.ts
+++ src/domain/verifiePalindrome.ts
@@ -1,16 +1,21 @@
 import * as os from "os";
 import {Langue} from "./langue";
 import {MomentJournee} from "./momentJournee";
+import {LangueFrancaise} from "./langueFrancaise"
 
 export class VerifiePalindrome {
+
+    // Constructeur class VerifiePalindrome
+    langue : Langue = new LangueFrancaise();
+    moment : MomentJournee = MomentJournee.Inconnu;
 
     public Miroir(chaine: string) : string {
         return chaine.split('').reverse().join('');
     }
-    public Verifie(chaine: string, langue : Langue) : string {
+    public Verifie(chaine: string) : string {
         let miroir = this.Miroir(chaine);
         if(miroir === chaine){
-            return miroir + os.EOL + langue.Valide();
+            return miroir + os.EOL + this.langue.Valide();
         }
         else {
             return miroir;
@@ -18,7 +23,7 @@ export class VerifiePalindrome {
     }
 
     public Console(chaine: string,  langue : Langue, moment : MomentJournee) : string {
-        let sortiePalindrome = this.Verifie(chaine, langue);
+        let sortiePalindrome = this.Verifie(chaine);
         return langue.Salue(moment) + os.EOL + sortiePalindrome + os.EOL + langue.Cloture(moment)
     }
 }
