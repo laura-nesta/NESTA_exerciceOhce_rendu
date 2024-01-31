9320fcbf8ad6c036964d7d9f25ec1dbdcc105560
laura-nesta <99044194+laura-nesta@users.noreply.github.com>
2024-02-01 00:57:27 +0100

    Delete build directory

--- build/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-};
