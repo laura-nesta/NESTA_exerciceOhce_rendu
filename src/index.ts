d49199fe6beb5a3a59b742876def11901b502491
laura-nesta <laura.nesta@outlook.fr>
2024-02-02 20:11:38 +0100

    add fonction langue systeme (ne marche pas)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import {MomentJournee} from "./domain/momentJournee";
 
+
 function main(){
     const moment: MomentJournee = DonneMomentJourne();
     console.log("Moment de la journée : " + moment)
+
+    const langueSysteme = DonneLangueSysteme()
+    console.log("Langage du systeme : " + langueSysteme);
 
 }
 
 function DonneMomentJourne() : MomentJournee{
     const heureActuelle = new Date().getHours();
     return MomentJournee.getMomentSelonHeure(heureActuelle);
+}
+
+function DonneLangueSysteme() {
+    // Je ne trouve pas de moyen de récupérer le langage du système
+    // const langueSysteme = navigator?.language  || 'Langue Française';
+    const langueSysteme =  'Langue Française';
+    return langueSysteme;
 }
 
 main();
